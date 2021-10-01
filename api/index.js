b9b722aec518fa162c96a26ae141e01ac047330d
Anand Simmy <anandsimmy7@gmail.com>
2021-10-01 08:34:01 +0530

    changed build folder

--- api/index.js
+++ api/index.js
@@ -65,10 +65,10 @@ wsServer.on('request', request => {
 })
 
 if (true || process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 };
 
