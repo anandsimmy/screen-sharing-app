6bd877ed3a575c44e4def39c5b9fb0e7a4698591
Anand Simmy <anandsimmy7@gmail.com>
2021-10-26 09:05:37 +0530

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react'
-import { setupConnection } from './connection-utils/connection';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CodeComponent from './components/CodeComponent/CodeComponent'
 import VideoComponent from './components/VideoComponent/VideoComponent'
 import styles from './App.module.css';
  
 const App= () => {
 
-  useEffect(()=> {
-    setupConnection();
-  }, [])
-
   return (
     <div className={styles.container}>
-        {/* <CodeComponent /> */}
-        <VideoComponent />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={CodeComponent} />
+          <Route path='/video' component={VideoComponent} />
+        </Switch>
+      </Router>
     </div>
   );
 }
