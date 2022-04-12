0a9626ea5b14e221a22234cc0faecd6605f17434
Anand Simmy <anandsimmy7@gmail.com>
2022-04-12 09:02:09 +0530

    code component changes

--- client/src/components/CodeComponent/CodeComponent.js
+++ client/src/components/CodeComponent/CodeComponent.js
@@ -1,11 +1,16 @@
-import React from 'react'
- 
-const CodeComponent= () => {
-    return (
-      <>
-      <input id='code' type='number' />
-      </>
-    )
-}
+import React, { useState } from 'react';
+import './CodeComponent.css';
 
-export default CodeComponent
+const CodeComponent = () => {
+  const { showJoin, setShowJoin } = useState(false);
+
+  return (
+    <div className='wrapper'>
+      <div className='title'>SHARE YOUR SCREEN</div>
+      <button className='button join'>Join Sharing</button>
+      <button className='button share'>Start Sharing</button>
+    </div>
+  );
+};
+
+export default CodeComponent;
